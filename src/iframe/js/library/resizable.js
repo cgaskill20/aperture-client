640773bb4248341d7fff8d07ada881374afabc8a
Jean-Marc Ruffalo-Burgat <me@jeanmarc.us>
2021-01-26 19:25:54 -0700

    working class

--- src/iframe/js/library/resizable.js
+++ src/iframe/js/library/resizable.js
@@ -4,100 +4,117 @@
 */
 class resizable {
     static numOfInstances = 0;
-    constructor(defaultWidth, defaultHeight, defaultX, defaultY, backgroundColor){
-        numOfInstances += 1;
+
+    constructor(defaultWidth, defaultHeight, backgroundColor){
+        resizable.numOfInstances += 1;
         this.width = defaultWidth;
         this.height = defaultHeight;
-        this.xCoordinate = defaultX;
-        this.yCoordinate = defaultY;
-        this.uniqueId = numOfInstances;
+        this.uniqueId = resizable.numOfInstances;
+        this.backgroundColor = backgroundColor;
         this.createOverlay();
+        this.addListeners(this.overlayDocument);
     }
 
     createOverlay(){
         const overlayDocument = document.createElement("div");
+        this.overlayDocument = overlayDocument;
         overlayDocument.id = "overlay" + this.uniqueId;
         overlayDocument.className = "overlay";
+        overlayDocument.style.width = this.width + "px";
+        overlayDocument.style.height = this.height + "px";
+        overlayDocument.style.background = this.backgroundColor;
 
         const boxDocument = document.createElement("div");
+        this.boxDocument = boxDocument;
         boxDocument.id = "box" + this.uniqueId;
         boxDocument.className = "box";
+
+        const boxResizer = document.createElement("div");
+        this.boxResizer = boxResizer;
+        boxResizer.id = "resizer" + this.uniqueId;
+        boxResizer.className = "resizer top-right";
+
+        boxDocument.appendChild(boxResizer);
+        overlayDocument.appendChild(boxDocument);
+        document.body.appendChild(overlayDocument);
+
     }
+
+    addListeners(overlayDocument){
+           var offset = [0,0];
+           var isDown = false;
+           var mousePosition;
+           const minimum_size = 20;
+
+           this.boxResizer.addEventListener('mousedown', function(e) {
+                isDown = false;
+                e.preventDefault();
+
+                let width =  parseFloat(getComputedStyle(overlayDocument, null).getPropertyValue('width').replace('px', ''));
+                let height = parseFloat(getComputedStyle(overlayDocument, null).getPropertyValue('height').replace('px', ''));
+                let yCoord = overlayDocument.getBoundingClientRect().top;
+                let mouseXCoord = e.pageX;
+                let mouseYCoord = e.pageY;
+                window.addEventListener('mousemove', resize);
+                window.addEventListener('mouseup', stopResize);
+
+               function resize(e) {
+                 let newWidth = width + (e.pageX - mouseXCoord)
+                 let newHeight = height - (e.pageY - mouseYCoord)
+                 if (newWidth > minimum_size) {
+                     overlayDocument.style.width = newWidth + 'px'
+                 }
+                 if (newHeight > minimum_size) {
+                     overlayDocument.style.height = newHeight + 'px'
+                     overlayDocument.style.top = yCoord + (e.pageY - mouseYCoord) + 'px'
+                 }
+               }
+
+               function stopResize() {
+                 window.removeEventListener('mousemove', resize)
+               }
+
+           })
+
+           overlayDocument.addEventListener('mousedown', function(e) {
+               isDown = true;
+               offset = [
+                   overlayDocument.offsetLeft - e.clientX,
+                   overlayDocument.offsetTop - e.clientY
+               ];
+
+
+           }, true);
+
+           overlayDocument.addEventListener('mouseup', function() {
+               isDown = false;
+           }, true);
+
+           window.addEventListener('mousemove', function(event) {
+               if (isDown) {
+                   mousePosition = {
+
+                       x : event.clientX,
+                       y : event.clientY
+
+                   };
+                   overlayDocument.style.left = (mousePosition.x + offset[0]) + 'px';
+                   overlayDocument.style.top  = (mousePosition.y + offset[1]) + 'px';
+               }
+           }, true);
+    }
+
 
 
 
 }
 
 
+
+
+
 // Run when page is loaded
  $( document ).ready(function() {
-   const element = document.querySelector('.overlay');
-   const corner = document.querySelector('.overlay .box .resizer');
-   const box = document.querySelector('.overlay');
-   var offset = [0,0];
-   var isDown = false;
-
-   var mousePosition;
-
-   // Minimum size so users dont completely lose the box
-   const minimum_size = 20;
-   corner.addEventListener('mousedown', function(e) {
-     isDown = false;
-       // Prevents a bug where map would think it was being highlighted
-       e.preventDefault();
-
-       let width =  parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
-       let height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
-       let yCoord = element.getBoundingClientRect().top;
-       let mouseXCoord = e.pageX;
-       let mouseYCoord = e.pageY;
-       window.addEventListener('mousemove', resize);
-       window.addEventListener('mouseup', stopResize);
-
-       function resize(e) {
-         let newWidth = width + (e.pageX - mouseXCoord)
-         let newHeight = height - (e.pageY - mouseYCoord)
-         if (newWidth > minimum_size) {
-             element.style.width = newWidth + 'px'
-         }
-         if (newHeight > minimum_size) {
-             element.style.height = newHeight + 'px'
-             element.style.top = yCoord + (e.pageY - mouseYCoord) + 'px'
-         }
-       }
-
-       function stopResize() {
-         window.removeEventListener('mousemove', resize)
-       }
-
-   })
-
-   box.addEventListener('mousedown', function(e) {
-       isDown = true;
-       offset = [
-           box.offsetLeft - e.clientX,
-           box.offsetTop - e.clientY
-       ];
-
-
-   }, true);
-
-   box.addEventListener('mouseup', function() {
-       isDown = false;
-   }, true);
-
-   window.addEventListener('mousemove', function(event) {
-       if (isDown) {
-           mousePosition = {
-
-               x : event.clientX,
-               y : event.clientY
-
-           };
-           box.style.left = (mousePosition.x + offset[0]) + 'px';
-           box.style.top  = (mousePosition.y + offset[1]) + 'px';
-       }
-   }, true);
-
+   const test = new resizable(500,100,"white");
 
  });
