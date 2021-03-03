da4b372263a3593f3a7c994c1f849966e704a5bd
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-03 09:46:53 -0700

    Dark Mode fully functional

--- src/iframe/js/frame-specific/darkMode.js
+++ src/iframe/js/frame-specific/darkMode.js
@@ -1,105 +1,22 @@
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
 var darkMode = true;
 
-var trueBlack = "#111";
-var softBlack = "#181818"
-var brightWhite = "#fbfbfb";
-var trueWhite = "#fff";
-var softWhite = "#f1f1f1";
+function switchTheme(e) {
 
-var bootstrapPrimary = "#007bff";
-var bootstrapWarning = "#ffc107";
-var bootstrapDark = "#343a40";
-var bootstrapLight = "#f8f9fa";
+	document.getElementById("sidebar-id").style.transition = "0.3s";
 
-var transitionTime = "0.3s";
-var realBorderRadius = "3px";
-var borderWhite = "1px solid #fbfbfb";
-var borderBlack = "1px solid #777";
-
-document.getElementById('nav-dark-mode-button').addEventListener('click', colorModeSwitch);
-function colorModeSwitch() {
-	var colorMode1 = document.getElementsByClassName("colorMode1");
-	var colorMode2 = document.getElementsByClassName("colorMode2");
-	var borders = document.getElementsByClassName("customBorder");
-	if(darkMode) {
-		for(var i = 0; i < colorMode1.length; i++) {
-			colorMode1[i].style.backgroundColor=trueBlack;
-			colorMode1[i].style.color=trueWhite;
-			colorMode1[i].style.transition=transitionTime;
-		}
-		for(var i = 0; i < colorMode2.length; i++) {
-			colorMode2[i].style.backgroundColor=softBlack;
-			colorMode2[i].style.color=trueWhite;
-			colorMode2[i].style.transition=transitionTime;
-		}
-		for(var i = 0; i < borders.length; i++) {
-			borders[i].style.border = borderWhite;
-			borders[i].style.borderRadius = realBorderRadius;
-		}
+	if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
 	}
 	else {
-		for(var i = 0; i < colorMode1.length; i++) {
-			colorMode1[i].style.backgroundColor=brightWhite;
-			colorMode1[i].style.color=trueBlack;
-			colorMode1[i].style.transition=transitionTime;
-		}
-		for(var i = 0; i < colorMode2.length; i++) {
-			colorMode2[i].style.backgroundColor=trueWhite;
-			colorMode2[i].style.color=softBlack;
-			colorMode2[i].style.transition=transitionTime;
-		}
-		for(var i = 0; i < borders.length; i++) {
-			borders[i].style.border = borderBlack;
-			borders[i].style.borderRadius = realBorderRadius;
-		}
-	}
-
-	var darkButtons = document.getElementsByClassName("btn-outline-dark");
-	if(darkMode) {
-		for(var i = 0; i < darkButtons.length; i++) {
-			darkButtons[i].style.color=bootstrapLight;
-			darkButtons[i].style.backgroundColor="transparent";
-			darkButtons[i].style.backgroundImage="none";
-			darkButtons[i].style.borderColor=bootstrapLight;
-			// darkButtons[i].style.hover.color="#212529";
-			// darkButtons[i].style.hover.backgroundColor="#f8f9fa";
-			// darkButtons[i].style.hover.borderColor="#f8f9fa";
-		}
-	}
-	else {
-		for(var i = 0; i < darkButtons.length; i++) {
-			darkButtons[i].style.color=bootstrapDark;
-			darkButtons[i].style.backgroundColor="transparent";
-			darkButtons[i].style.backgroundImage="none";
-			darkButtons[i].style.borderColor=bootstrapDark;
-			// darkButtons[i].style.hover.color="#fff";
-			// darkButtons[i].style.hover.backgroundColor="#343a40";
-			// darkButtons[i].style.hover.borderColor="#343a40";
-		}
-	}
-
-	//FIXME Get these working
-	var toolTips = document.getElementsByClassName("tooltip-inner");
-	if(darkMode) {
-		for(var i = 0; i < toolTips.length; i++) {
-			toolTips[i].style.color=trueBlack;
-			toolTips[i].style.backgroundColor=trueWhite;
-			toolTips[i].style.border=borderWhite;
-		}
-	}
-	else {
-		for(var i = 0; i < toolTips.length; i++) {
-			toolTips[i].style.color=brightWhite;
-			toolTips[i].style.backgroundColor=trueBlack;
-			toolTips[i].style.border=borderBlack;
-		}
+		document.documentElement.setAttribute('data-theme', 'light');
 	}
 
 	var dropdownArrow = document.getElementsByClassName("dropdown-arrow");
 	var tooltipIcon = document.getElementsByClassName("tool-tip");
-	var graph = document.getElementsByClassName("graph-button-pic");
-	var darkModeButton = document.getElementsByClassName("dark-mode-button");
+	var graphIcon = document.getElementsByClassName("graph-button-pic");
 	if(darkMode) {
 		for(var i = 0; i < dropdownArrow.length; i++) {
 	 		dropdownArrow[i].src="../../images/Dropdown_White.png";
@@ -107,11 +24,8 @@ function colorModeSwitch() {
 		for(var i = 0; i < tooltipIcon.length; i++) {
 			tooltipIcon[i].src="../../images/Info_White.png";
 		}
-		for(var i = 0; i < graph.length; i++) {
-			graph[i].src="../../images/Graph_White.png";
-		}
-		for(var i = 0; i < darkModeButton.length; i++) {
-			darkModeButton[i].src="../../images/Moon.png";
+		for(var i = 0; i < graphIcon.length; i++) {
+			graphIcon[i].src="../../images/Graph_White.png";
 		}
 	}
 	else {
@@ -121,13 +35,12 @@ function colorModeSwitch() {
 		for(var i = 0; i < tooltipIcon.length; i++) {
 			tooltipIcon[i].src="../../images/Info_Black.png";
 		}
-		for(var i = 0; i < graph.length; i++) {
-			graph[i].src="../../images/Graph_Black.png";
-		}
-		for(var i = 0; i < darkModeButton.length; i++) {
-			darkModeButton[i].src="../../images/Sun.png";
+		for(var i = 0; i < graphIcon.length; i++) {
+			graphIcon[i].src="../../images/Graph_Black.png";
 		}
 	}
 
-    darkMode = !darkMode;
+	darkMode = !darkMode;
 }
+
+toggleSwitch.addEventListener('change', switchTheme, false);
