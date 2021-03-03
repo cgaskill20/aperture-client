e435965259ce4faf41c826b13261a2fbdd1b4ca1
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-03 10:20:29 -0700

    Cleaned up

--- src/iframe/js/frame-specific/darkMode.js
+++ src/iframe/js/frame-specific/darkMode.js
@@ -1,10 +1,5 @@
-
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-var darkMode = true;
-
 function switchTheme(e) {
-
 	document.getElementById("sidebar-id").style.transition = "0.3s";
 
 	if (e.target.checked) {
@@ -13,34 +8,6 @@ function switchTheme(e) {
 	else {
 		document.documentElement.setAttribute('data-theme', 'light');
 	}
-
-	var dropdownArrow = document.getElementsByClassName("dropdown-arrow");
-	var tooltipIcon = document.getElementsByClassName("tool-tip");
-	var graphIcon = document.getElementsByClassName("graph-button-pic");
-	if(darkMode) {
-		for(var i = 0; i < dropdownArrow.length; i++) {
-	 		dropdownArrow[i].src="../../images/Dropdown_White.png";
-		}
-		for(var i = 0; i < tooltipIcon.length; i++) {
-			tooltipIcon[i].src="../../images/Info_White.png";
-		}
-		for(var i = 0; i < graphIcon.length; i++) {
-			graphIcon[i].src="../../images/Graph_White.png";
-		}
-	}
-	else {
-		for(var i = 0; i < dropdownArrow.length; i++) {
-			dropdownArrow[i].src="../../images/Dropdown_Black.png";
-		}
-		for(var i = 0; i < tooltipIcon.length; i++) {
-			tooltipIcon[i].src="../../images/Info_Black.png";
-		}
-		for(var i = 0; i < graphIcon.length; i++) {
-			graphIcon[i].src="../../images/Graph_Black.png";
-		}
-	}
-
-	darkMode = !darkMode;
+	switchImageSources();
 }
-
 toggleSwitch.addEventListener('change', switchTheme, false);
