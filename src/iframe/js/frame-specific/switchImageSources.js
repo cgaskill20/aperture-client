9f4d71c6b26b18574cad34a94ca10e7370023f7b
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-04 18:49:58 -0700

    ...and the new image files

---
+++ src/iframe/js/frame-specific/switchImageSources.js
@@ -0,0 +1,30 @@
+var darkMode = true;
+function switchImageSources() {
+	var dropdownArrow = document.getElementsByClassName("dropdown-arrow");
+	var tooltipIcon = document.getElementsByClassName("tool-tip");
+	var moonIcon = document.getElementsByClassName("moon-icon");
+	if(darkMode) {
+		for(var i = 0; i < dropdownArrow.length; i++) {
+	 		dropdownArrow[i].src="../../images/Dropdown_White.png";
+		}
+		for(var i = 0; i < tooltipIcon.length; i++) {
+			tooltipIcon[i].src="../../images/Info_White.png";
+		}
+		for(var i = 0; i < moonIcon.length; i++) {
+			// moonIcon[i].src="../../images/Moon_Purple&Yellow.png";
+			moonIcon[i].src="../../images/Moon_White.png";
+		}
+	}
+	else {
+		for(var i = 0; i < dropdownArrow.length; i++) {
+			dropdownArrow[i].src="../../images/Dropdown_Black.png";
+		}
+		for(var i = 0; i < tooltipIcon.length; i++) {
+			tooltipIcon[i].src="../../images/Info_Black.png";
+		}
+		for(var i = 0; i < moonIcon.length; i++) {
+			moonIcon[i].src="../../images/Moon_Black.png";
+		}
+	}
+	darkMode = !darkMode;
+}
