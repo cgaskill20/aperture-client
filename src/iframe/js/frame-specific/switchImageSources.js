1fd52cc60eb8569d8d3756464146961e5860cca0
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-06 08:47:05 -0700

    Image Source

--- src/iframe/js/frame-specific/switchImageSources.js
+++ src/iframe/js/frame-specific/switchImageSources.js
@@ -1,27 +1,28 @@
-var darkMode = true;
+let darkMode = true;
 function switchImageSources() {
-	var dropdownArrow = document.getElementsByClassName("dropdown-arrow");
-	var tooltipIcon = document.getElementsByClassName("tool-tip");
-	var moonIcon = document.getElementsByClassName("moon-icon");
+	let dropdownArrow = document.getElementsByClassName("dropdown-arrow");
+	let tooltipIcon = document.getElementsByClassName("tool-tip");
+	let moonIcon = document.getElementsByClassName("moon-icon");
 	if(darkMode) {
-		for(var i = 0; i < dropdownArrow.length; i++) {
+		// dropdownArrow.forEach(element => element.src="../../images/Dropdown_White.png");
+		for(let i = 0; i < dropdownArrow.length; i++) {
 	 		dropdownArrow[i].src="../../images/Dropdown_White.png";
 		}
-		for(var i = 0; i < tooltipIcon.length; i++) {
+		for(let i = 0; i < tooltipIcon.length; i++) {
 			tooltipIcon[i].src="../../images/Info_White.png";
 		}
-		for(var i = 0; i < moonIcon.length; i++) {
+		for(let i = 0; i < moonIcon.length; i++) {
 			moonIcon[i].src="../../images/Moon_White.png";
 		}
 	}
 	else {
-		for(var i = 0; i < dropdownArrow.length; i++) {
+		for(let i = 0; i < dropdownArrow.length; i++) {
 			dropdownArrow[i].src="../../images/Dropdown_Black.png";
 		}
-		for(var i = 0; i < tooltipIcon.length; i++) {
+		for(let i = 0; i < tooltipIcon.length; i++) {
 			tooltipIcon[i].src="../../images/Info_Black.png";
 		}
-		for(var i = 0; i < moonIcon.length; i++) {
+		for(let i = 0; i < moonIcon.length; i++) {
 			moonIcon[i].src="../../images/Moon_Black.png";
 		}
 	}
