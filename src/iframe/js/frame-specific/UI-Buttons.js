97a33a62a57766351499724ac920c9769bd4c955
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-20 09:47:49 -0600

    Cleaning up CSS and JS, organizing

--- src/iframe/js/frame-specific/UI-Buttons.js
+++ src/iframe/js/frame-specific/UI-Buttons.js
@@ -4,16 +4,9 @@ map.on('click', function () {
 
 document.getElementById('nav-menu-button').addEventListener('click', openNav);
 document.getElementById('nav-close-button').addEventListener('click', closeNav);
-
-$('#nav-data-exploration-button').on('click', function(e) {
-    document.getElementById("sidebar-container").style.display = "grid";
-});
-$('#nav-modeling-button').on('click', function(e) {
-    document.getElementById("sidebar-container").style.display = "none";
-});
-$('#nav-validation-button').on('click', function(e) {
-    document.getElementById("sidebar-container").style.display = "none";
-});
+document.getElementById('nav-data-exploration-button').addEventListener('click', showDataExploration);
+document.getElementById('nav-modeling-button').addEventListener('click', showModeling);
+// document.getElementById('nav-validation-button').addEventListener('click', showValidation);
 
 function openNav() {
   document.getElementById("sidebar-id").style.width = "52vw";
@@ -25,4 +18,25 @@ function closeNav() {
   document.getElementById("sidebar-id").style.width = "0";
   document.getElementById("sidebar-id").style.transition = "0.7s";
   document.getElementById("main").style.opacity = "1";
+}
+
+function showDataExploration() {
+    document.getElementById("sidebar-container").style.display = "grid";
+    document.getElementById("model-container").style.display = "none";
+    document.getElementById("clusterLegendContainer").style.display = "none";
+    map.removeLayer(dataModelingGroup)
+    map.addLayer(dataExplorationGroup)
+}
+
+function showModeling() {
+    document.getElementById("sidebar-container").style.display = "none";
+    document.getElementById("model-container").style.display = "block";
+    document.getElementById("clusterLegendContainer").style.display = "block";
+    map.addLayer(dataModelingGroup)
+    map.removeLayer(dataExplorationGroup)
+}
+
+function showValidation() {
+    document.getElementById("sidebar-container").style.display = "none";
+    document.getElementById("model-container").style.display = "none";
 }
--- src/iframe/js/frame-specific/map2.js
+++ src/iframe/js/frame-specific/map2.js
@@ -95,46 +95,9 @@ window.backgroundTract = backgroundTract;
 window.backgroundCounty = backgroundCounty;
 
 
-map.on('click', function () {
-    closeNav();
-});
-
-document.getElementById('nav-menu-button').addEventListener('click', openNav);
-document.getElementById('nav-close-button').addEventListener('click', closeNav);
-document.getElementById('nav-data-exploration-button').addEventListener('click', showDataExploration);
-document.getElementById('nav-modeling-button').addEventListener('click', showModeling);
-document.getElementById('nav-validation-button').addEventListener('click', showValidation);
-
-function openNav() {
-    document.getElementById("sidebar-id").style.width = "52vw";
-    document.getElementById("main").style.opacity = "0";
-}
-
-function closeNav() {
-    document.getElementById("sidebar-id").style.width = "0";
-    document.getElementById("main").style.opacity = "1";
-}
-
-function showDataExploration() {
-    document.getElementById("sidebar-container").style.display = "grid";
-    document.getElementById("model-container").style.display = "none";
-    document.getElementById("clusterLegendContainer").style.display = "none";
-    map.removeLayer(dataModelingGroup)
-    map.addLayer(dataExplorationGroup)
-}
-
-function showModeling() {
-    document.getElementById("sidebar-container").style.display = "none";
-    document.getElementById("model-container").style.display = "block";
-    document.getElementById("clusterLegendContainer").style.display = "block";
-    map.addLayer(dataModelingGroup)
-    map.removeLayer(dataExplorationGroup)
-}
-
-function showValidation() {
-    document.getElementById("sidebar-container").style.display = "none";
-    document.getElementById("model-container").style.display = "none";
-}
+// map.on('click', function () {
+//     closeNav();
+// });
 
 const overwrite = { //leaving this commented cause it explains the schema really well 
     // "covid_county": {
