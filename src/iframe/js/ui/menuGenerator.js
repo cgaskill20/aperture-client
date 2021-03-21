4caea9e4cb89a811b8221fce8ad3346906c79964
Daniel Reynolds <danielreynolds101@gmail.com>
2021-03-21 12:51:29 -0600

    fix and make sliders work

--- src/iframe/js/ui/menuGenerator.js
+++ src/iframe/js/ui/menuGenerator.js
@@ -200,7 +200,7 @@ const MenuGenerator = {
             };
             layerObj["onUpdate"] = function () { layerQuerier.query(); };
             layerObj["onAdd"] = function () { layerQuerier.onAdd(); };
-            layerObj["onRemove"] = function () { layerQuerier.onRemove(); };
+            layerObj["onRemove"] = function () { layerQuerier.onRemove(); this.resetConstraintsForLayer(layerName)}.bind(this);
         }
 
         const onAdd = layerObj["onAdd"];
@@ -242,6 +242,10 @@ const MenuGenerator = {
             layerSelector.appendChild(this.createDropdown(layerConstraints));
         }
         return layerContainer;
+    },
+
+    resetConstraintsForLayer: function(layerName){
+        document.dispatchEvent(new CustomEvent(`${layerName}_reset_constraints`));
     },
 
     createModal: function (layerLabel, layerConstraints, layerQuerier, constraintsObj, layerInfo) {
@@ -371,7 +375,6 @@ const MenuGenerator = {
     },
 
     createSliderContainer: function (constraint, constraintObj, layerObj, layerName) {
-
         const sliderContainer = document.createElement("div");
         sliderContainer.className = "slider-individual";
         sliderContainer.id = constraint;
@@ -400,10 +403,16 @@ const MenuGenerator = {
                 sliderLabel.innerHTML += " - " + (isDate ? (new Date(Number(values[i]))).toUTCString().substr(0, 16) : (step < 1 ? values[i] : Math.floor(values[i])));
             }
         });
+
+        //listen for reset
+        document.addEventListener(`${layerName}_reset_constraints`, () => {
+            slider.noUiSlider.reset();
+        });
+
         const onConstraintChange = layerObj['onConstraintChange'];
         if (onConstraintChange) {
             onConstraintChange(layerName, constraint, slider.noUiSlider.get());
-            slider.noUiSlider.on('change', function (values) {
+            slider.noUiSlider.on('set', function (values) {
                 onConstraintChange(layerName, constraint, values);
             });
         }
@@ -434,7 +443,12 @@ const MenuGenerator = {
                 const checkboxSelector = document.createElement("input");
                 checkboxSelector.type = type;
                 checkboxSelector.id = Util.spaceToUnderScore(option);
-                checkboxSelector.checked = type === "radio" ? isFirstCheckbox : true;
+                const defaultChecked = type === "radio" ? isFirstCheckbox : true;
+                checkboxSelector.checked = defaultChecked;
+                //listen for reset
+                document.addEventListener(`${layerName}_reset_constraints`, () => {
+                    checkboxSelector.checked = defaultChecked;
+                });
                 checkboxSelector.name = constraint;
                 isFirstCheckbox = false;
                 const labelForRadioSelector = document.createElement("label");
