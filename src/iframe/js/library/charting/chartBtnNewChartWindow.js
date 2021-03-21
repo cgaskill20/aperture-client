dbf205fb9937bb7f109081504e35d4ed382378ea
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-21 16:42:45 -0600

    Some refactoring

--- src/iframe/js/library/charting/chartBtnNewChartWindow.js
+++ src/iframe/js/library/charting/chartBtnNewChartWindow.js
@@ -14,37 +14,48 @@ function createAddChartArea() {
     let addGraphBox = document.createElement("div");
     addGraphBox.className = "colorMode1 add-graph-box";
     addGraphBox.id = "graph-controller";
+    addGraphBox.appendChild(makeGraphButtonGroup());
+    document.getElementById('box1').appendChild(addGraphBox);
+}
 
+function makeGraphButtonGroup() {
     let graphButtonAreaDiv = document.createElement("div");
     graphButtonAreaDiv.className = "row justify-content-center";
+
     let graphButtonArea = document.createElement("div");
     graphButtonArea.className = "btn-group justify-content-center";
     graphButtonArea.setAttribute("role", "group");
-    let histogramButton = document.createElement("button");
-    histogramButton.className = "btn btn-outline-dark";
-    histogramButton.id = "histogram-button-id";
-    histogramButton.innerText = "Histogram";
-    let scatterplotButton = document.createElement("button");
-    scatterplotButton.className = "btn btn-outline-dark";
-    scatterplotButton.id = "scatterplot-button-id";
-    scatterplotButton.innerText = "Scatterplot";
-    let lineGraphButton = document.createElement("button");
-    lineGraphButton.className = "btn btn-outline-dark";
-    lineGraphButton.id = "linegraph-button-id";
-    lineGraphButton.innerText = "Line Graph";
-    let closeGraphButton = document.createElement("button");
-    closeGraphButton.className = "btn btn-outline-dark";
-    closeGraphButton.innerText = "Close";
-    closeGraphButton.addEventListener('click', showGraph);
-    lineGraphButton.disabled = true; //FIXME Remove this line once lineGraphs are implemented
+    graphButtonAreaDiv.appendChild(graphButtonArea);
+
+    let histogramButton = makeGraphButton("histogram-button-id", "Histogram");
+    let scatterplotButton = makeGraphButton("scatterplot-button-id", "Scatterplot");
+    let lineGraphButton = makeGraphButton("linegraph-button-id", "Line Graph", true);
+    let closeGraphButton = makeCloseButton();
 
     graphButtonArea.appendChild(histogramButton);
     graphButtonArea.appendChild(scatterplotButton);
     graphButtonArea.appendChild(lineGraphButton);
     graphButtonArea.appendChild(closeGraphButton);
     graphButtonAreaDiv.appendChild(graphButtonArea);
-    addGraphBox.appendChild(graphButtonAreaDiv);
-    document.getElementById('box1').appendChild(addGraphBox);
+
+    return graphButtonAreaDiv;
+}
+
+function makeGraphButton(id, name, disable = false) {
+    let button = document.createElement("button");
+    button.className = "btn btn-outline-dark";
+    button.id = id;
+    button.innerText = name;
+    button.disabled = disable;
+    return button;
+}
+
+function makeCloseButton() {
+    let closeGraphButton = document.createElement("button");
+    closeGraphButton.className = "btn btn-outline-dark";
+    closeGraphButton.innerText = "Close";
+    closeGraphButton.addEventListener('click', showGraph);
+    return closeGraphButton;
 }
 
 function makeButtonsWork() {
