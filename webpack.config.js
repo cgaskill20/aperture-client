d312361c40f4513d7d5fa0472a370b9264db4649
Daniel Reynolds <danielreynolds101@gmail.com>
2021-04-01 22:05:23 -0600

    clean webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,7 @@ module.exports = {
         test: /\.js$/, 
         exclude: /node_modules/, 
         use: [{
-          loader: 'babel-loader',
-          options: {
-              presets: ["@babel/preset-react"],
-              plugins: ["@babel/plugin-proposal-class-properties"]
-          }
+          loader: 'babel-loader'
       }] 
       },
       {
