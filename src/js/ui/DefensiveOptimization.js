91d1658bdc9b43f1fe4da0475c502e929d3ffb26
Daniel Reynolds <danielreynolds101@gmail.com>
2021-04-13 21:22:27 -0600

    suspicious programming practices

--- src/js/ui/DefensiveOptimization.js
+++ src/js/ui/DefensiveOptimization.js
@@ -5,7 +5,7 @@ import AutoQuery from '../library/autoQuery';
 function renderAlert(blocker,text) {
     if (blocker) {
         return <Alert color={"danger"} style={{
-            width: "430px"
+            width: "445px"
         }}>
             {text}
         </Alert>
@@ -18,7 +18,16 @@ export default function DefensiveOptimization(props) {
         AutoQuery.setBlockerListener((newBlockers) => { setBlockers({...newBlockers}) });
     });
 
-    return <div className={"warningContainer"}>
+    const HEIGHT_PER_ALERT = 65;
+    const height = (Object.values(blockers)
+        .map(curr => {return Number(curr)})
+        .reduce((acc,curr) => {
+            return acc + curr;
+        }, 0) * HEIGHT_PER_ALERT).toString() + "px";
+
+    return <div className={"warningContainer"} style={{
+        height: height
+    }}>
         {renderAlert(blockers.tract, "Can't Query Tracts at This Zoom Level, Please Zoom In")}
         {renderAlert(blockers.county, "Can't Query Counties at This Zoom Level, Please Zoom In")}
     </div>
