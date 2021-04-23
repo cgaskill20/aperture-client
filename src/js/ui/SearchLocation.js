fbac50c43c013aeb54bf86c5bdd575790c6d73fa
Daniel Reynolds <danielreynolds101@gmail.com>
2021-04-22 22:29:00 -0600

    working search bar lmao

--- src/js/ui/SearchLocation.js
+++ src/js/ui/SearchLocation.js
@@ -1,6 +1,7 @@
-import React, { Component, useEffect, useState } from 'react';
-import { Button } from 'reactstrap';
+import React, { Component, useEffect, useRef, useState } from 'react';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button } from 'reactstrap';
 import currentLocationIcon from '../../../images/current-location.svg';
+import L from 'leaflet';
 
 export function goToLocation(pos, map) {
     map.flyTo({
@@ -9,10 +10,34 @@ export function goToLocation(pos, map) {
     });
 }
 
+const GEOCODING_URL = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=pjson&outFields=Addr_type&forStorage=false&SingleLine=";
+async function geocode(search) {
+    return new Promise(resolve => {
+        fetch(`${GEOCODING_URL}${search}`)
+            .then((response) => {
+                resolve(response.json());
+            })
+    });
+}
+
 export default function SearchLocation(props) {
-    return <Button outline color={"dark"} className={"currentLocationButton"} onClick={() => {
-        navigator.geolocation.getCurrentPosition(pos => { goToLocation(pos.coords, props.map) }, err => { console.error(err) });
-    }}>
-        <img src={currentLocationIcon} className={"icon"} />
-    </Button>;
+    const locationSearchRef = useRef(null);
+    return <InputGroup>
+        <InputGroupAddon addonType="prepend">
+            <Button onClick={async () => {
+                const search = locationSearchRef.current.value;
+                if (search.length) {
+                    const addrRes = await geocode(search);
+                    console.log(addrRes)
+                    const { xmin, xmax, ymin, ymax } = addrRes.candidates[0].extent;
+                    props.map.flyToBounds(L.latLngBounds(L.latLng(ymin, xmin),L.latLng(ymax, xmax)));
+                    
+                }
+            }
+            }>
+                🔍
+            </Button>
+        </InputGroupAddon>
+        <Input innerRef={locationSearchRef} placeholder="Search for a location..." />
+    </InputGroup>;
 }
