bab946282b02519679826a6710338ffd9ae49b08
Daniel Reynolds <danielreynolds101@gmail.com>
2021-05-17 15:14:23 -0600

    made query file

---
+++ src/js/library/Query.js
@@ -0,0 +1,22 @@
+/**
+ * @class Query
+ * @file Makes queries and returns them
+ * @author Daniel Reynolds
+ */
+
+const Query = {
+    init: (queryableData) => {
+        console.log(queryableData)
+    },
+
+    /**
+      * Makes a query
+      * @memberof Query
+      * @param {JSON} query JSON that matches a query schema
+      */
+    makeQuery: async (query) => {
+
+    }
+}
+
+export default Query;
