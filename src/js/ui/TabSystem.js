2c3558a21ac3f822e0f18d4607520389267acd04
Matt Young <honeybadgerofdoom@gmail.com>
2021-05-18 15:26:52 -0600

    Adding checkboxes/sliders

--- src/js/ui/TabSystem.js
+++ src/js/ui/TabSystem.js
@@ -6,6 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import DataExploration from "./DataExploration/DataExploration";
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -71,7 +72,7 @@ export default function TabSystem() {
                 </Tabs>
             </Paper>
             <TabPanel value={value} index={0}>
-                Data Exploration
+                <DataExploration/>
             </TabPanel>
             <TabPanel value={value} index={1}>
                 Modeling
