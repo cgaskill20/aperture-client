23a1c075313cfb83de33248f91f8a45465b7822c
Matt Young <honeybadgerofdoom@gmail.com>
2021-05-21 13:59:14 -0600

    formatting

--- src/js/ui/NewDataExploration/NewDataExploration.js
+++ src/js/ui/NewDataExploration/NewDataExploration.js
@@ -44,8 +44,6 @@ function a11yProps(index) {
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-        width: '100%',
-        backgroundColor: theme.palette.background.paper,
     },
 }));
 
