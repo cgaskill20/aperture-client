478363e7f71c38956b53f7d03daf28b9694a579b
psmith36 <psmith36@rams.colostate.edu>
2021-05-25 16:13:29 -0600

    convert ChartFrameComponent to hooks

--- src/js/ui/NewCharting/ChartFrameComponent.js
+++ src/js/ui/NewCharting/ChartFrameComponent.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import { ChartingType } from '../../library/charting/chartSystem'
 
-export default class ChartFrameComponent extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div style={{
-                width: "100%",
-                height: "300px",
-                backgroundColor: "#eee",
-                border: "1px solid red"
-            }}>
-                <p>{this.props.type}</p>
-            </div>
-        );
-    }
+export default function ChartFrameComponent(props) {
+    return (
+        <div style={{
+            width: "100%",
+            height: "300px",
+            backgroundColor: "#eee",
+            border: "1px solid red"
+        }}>
+            <p>{props.type}</p>
+        </div>
+    );
 }
