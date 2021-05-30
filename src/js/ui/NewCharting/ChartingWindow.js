0d32e97313181dea8c1bb78f36fbae7fd976ac04
psmith36 <psmith36@rams.colostate.edu>
2021-05-30 15:51:00 -0600

    proper frame setup

--- src/js/ui/NewCharting/ChartingWindow.js
+++ src/js/ui/NewCharting/ChartingWindow.js
@@ -4,13 +4,14 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Grid from '@material-ui/core/Grid';
 import ChartFrameComponent from './ChartFrameComponent';
-import makeFrame from './makeFrame';
+import Frame from './makeFrame';
 
 export default function ChartingWindow(props) {
     const [frames, setFrames] = useState([]);
 
-    const addChartFrame = (frame) => {   
-        setFrames(frames.concat([frame]));
+    const addChartFrame = frame => {   
+        setFrames(frames.concat([<Frame type={"histogram"} size={props.size}/>]));
+        console.log(frames);
     }
 
     return (
@@ -18,10 +19,7 @@ export default function ChartingWindow(props) {
             <Grid container direction="row" alignItems="center" justify="center" spacing={1}>
                 <Grid container direction="column" alignItems="center" justify="center" style={{ width: "90%" }}>
                     <ChartGlobalControls make={addChartFrame}/>
-                    {frames.map((frame, index) => {
-                        const Frame = makeFrame(frame.type);
-                        return <Frame key={index} size={props.size}/>;
-                    })}
+                        {frames}
                 </Grid>
             </Grid>
         </Grid>
--- src/js/ui/NewCharting/makeFrame.js
+++ src/js/ui/NewCharting/makeFrame.js
@@ -3,23 +3,22 @@ import { ChartingType, DataSourceType } from '../../library/charting/chartSystem
 import ChartFrameComponent from "./ChartFrameComponent";
 import Histogram from "./Histogram";
 
-export default function makeFrame(type) {
-    return function(props) {
-        const [id, setID] = useState(`${type.name}-frame-${Math.random().toString(36).substring(2, 6)}`);
+export default function Frame(props) {
+    const [id, setID] = useState(`${props.type.name}-frame-${Math.random().toString(36).substring(2, 6)}`);
+    console.log("in root function");
 
-        useEffect(() => {
-            window.chartSystem.registerChart(id, type);
-            return () => window.chartSystem.unregisterChart(id); 
-        }, []);
+    useEffect(() => {
+        window.chartSystem.registerChart(id, props.type);
+        return () => window.chartSystem.unregisterChart(id); 
+    }, []);
 
-        return (
-            <div style={{
-                width: "100%",
-                height: props.size.height - 50,
-                border: "1px solid red"
-            }}>
-                <Histogram size={props.size}></Histogram>
-            </div>
-        );
-    }
+    return (
+        <div style={{
+            width: "100%",
+            height: props.size.height - 50,
+            border: "1px solid red"
+        }}>
+            <Histogram size={props.size}></Histogram>
+        </div>
+    );
 }
