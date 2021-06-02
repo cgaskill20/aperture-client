d8f3c1098973df765d84cdd3536d1b1e028d7062
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-01 22:44:18 -0600

    added xyz lib

--- src/js/library/newRendering.js
+++ src/js/library/newRendering.js
@@ -1,1 +1,7 @@
 import geojsonvt from 'geojson-vt';
+import xyz from 'xyz-affair';
+
+export default function newRendering(geojson) {
+    //const tiles = xyz(globalThis.map.getBounds(),)
+    var tileIndex = geojsonvt(geojson);
+}
