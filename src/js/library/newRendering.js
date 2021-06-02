3fae3b2ed23882e5243b6edc4a113f91d7e0cf5e
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-02 11:56:37 -0600

    set up more canvas code

--- src/js/library/newRendering.js
+++ src/js/library/newRendering.js
@@ -9,11 +9,11 @@ export default function newRendering(geojson) {
     jsonArr.push(geojson)
 }
 
-export function getFeatures(){
+export function getFeatures() {
     const tiles = getTiles();
     tileIndex = geojsonvt(Util.createGeoJsonObj(jsonArr));
     let allbits = []
-    for(const {x,y,z} of tiles){
+    for (const { x, y, z } of tiles) {
         //console.log(tileIndex)
         const bits = tileIndex.getTile(z, x, y)?.features
         allbits = [...allbits, bits]
@@ -33,37 +33,45 @@ const getTiles = () => {
             leafletBounds.getNorth()
         ]
     ]
-    return xyz(bounds,globalThis.map.getZoom())
+    return xyz(bounds, globalThis.map.getZoom())
 }
 
 setInterval(() => {
     //console.log({features: getFeatures()})
-},10*1000)
+    tileIndex = geojsonvt(Util.createGeoJsonObj(jsonArr));
+}, 3 * 1000)
 
 document.onkeypress = function (e) {
     e = e || window.event;
-    if(e.keyCode === 114){
-        for(const coords of getTiles()){
-            createLayer(coords)
-        }
+    if (e.keyCode === 114) {
+        createLayer();
     }
 };
 
-const createLayer = coords => {
-    // ...
+const createLayer = () => {
+    console.log("here")
     const CanvasLayer = GridLayer.extend({
-      createTile: function(coords) { // 
-        // console.log(coords); { x: Number, y: Number, z: Number }
-        // leaflet will run this method for each tile needed in the viewport
-        // create a canvas (we will use this canvas to draw our features)
-        let tile = DomUtil.create('canvas', 'leaflet-tile leaflet-sedesol');
-        // Set the tile size
-        tile.width = 256;
-        tile.height = 256;
-        // at the moment we will just return an empty canvas
-        return tile;
-      }
+        createTile: function ({ x, y, z }) {
+            let tile = DomUtil.create('canvas', 'leaflet-tile leaflet-sedesol');
+            const ctx = tile.getContext('2d');
+            // Set the tile size
+            tile.width = 256;
+            tile.height = 256;
+            const tileToRender = tileIndex.getTile(z, x, y);
+            ctx.clearRect(0, 0, tile.width, tile.height);
+            // If tileToRender is null return just a clear canvas
+            if (!tileToRender) {
+                return tile;
+            }
+
+            const { features } = tileToRender;
+            ctx.strokeStyle = 'grey';
+            features.forEach(feature => {
+                const geometries = feature;
+                console.log(geometry)
+            })
+        }
     });
     // Add layer to the canvas
     globalThis.map.addLayer(new CanvasLayer());
-  };
+};
