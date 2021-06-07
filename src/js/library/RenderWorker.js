26ae04fa7bb0042d5cec2814953a4fa36ad66f62
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-07 13:44:36 -0600

    more internals

--- src/js/library/RenderWorker.js
+++ src/js/library/RenderWorker.js
@@ -17,30 +17,46 @@ const tryUpdate = () => {
 
 setInterval(() => {
     tryUpdate();
-}, 500) 
+}, 500)
 
 const addOne = (newFeature) => {
-    !new Set(featureArr.map(feature => feature.id)).has(newFeature.id) && featureArr.push(newFeature);
+    if (!new Set(featureArr.map(feature => feature.id)).has(newFeature.id)) {
+        featureArr.push(newFeature);
+        shouldUpdate = true;
+    }
 }
 
 const addMultiple = (newFeatures) => {
-    for(const newFeature of newFeatures){
+    for (const newFeature of newFeatures) {
         addOne(newFeature);
     }
 }
 
+const removeOne = (idToRemove) => {
+    removeMultiple([idToRemove]);
+}
+
+const removeMultiple = (idsToRemove) => {
+    featureArr.filter(feature => !idsToRemove.includes(feature.id));
+}
+
 onmessage = function (msg) {
-    const { type, coords, toAdd } = msg.data;
+    const { type, coords, toAdd, toRemove } = msg.data;
     if (type === "add") {
-        if(Array.isArray(toAdd)){
+        if (Array.isArray(toAdd)) {
             addMultiple(toAdd);
         }
-        else{
+        else {
             addOne(toAdd);
         }
     }
     else if (type === "remove") {
-        
+        if (Array.isArray(toRemove)) {
+            removeMultiple(toRemove);
+        }
+        else {
+            removeOne(toRemove);
+        }
     }
     else if (type === "get") {
         const { x, y, z } = coords;
