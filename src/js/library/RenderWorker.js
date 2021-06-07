eb7e82ca2bfda2e577701e883738e3ad0189364e
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-07 13:36:28 -0600

    added add

--- src/js/library/RenderWorker.js
+++ src/js/library/RenderWorker.js
@@ -2,20 +2,42 @@ import geojsonvt from 'geojson-vt';
 import Util from './apertureUtil';
 
 let featureArr = [];
-let updateFeatures = false;
+let shouldUpdate = false;
 
 let tileIndex;
 const update = () => {
     tileIndex = geojsonvt(Util.createGeoJsonObj(featureArr));
+    shouldUpdate = false;
 }
 update();
 
+const tryUpdate = () => {
+    shouldUpdate && update();
+}
+
+setInterval(() => {
+    tryUpdate();
+}, 500) 
+
+const addOne = (newFeature) => {
+    !new Set(featureArr.map(feature => feature.id)).has(newFeature.id) && featureArr.push(newFeature);
+}
+
+const addMultiple = (newFeatures) => {
+    for(const newFeature of newFeatures){
+        addOne(newFeature);
+    }
+}
 
 onmessage = function (msg) {
-    const { type, coords } = msg.data;
+    const { type, coords, toAdd } = msg.data;
     if (type === "add") {
-
-        
+        if(Array.isArray(toAdd)){
+            addMultiple(toAdd);
+        }
+        else{
+            addOne(toAdd);
+        }
     }
     else if (type === "remove") {
         
