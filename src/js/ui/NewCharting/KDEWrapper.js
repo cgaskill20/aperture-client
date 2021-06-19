ac90da1ffbef85b9244745beafe4ed51a23e7c57
psmith36 <psmith36@rams.colostate.edu>
2021-06-18 20:02:09 -0600

    add clamping and blur handling

--- src/js/ui/NewCharting/KDEWrapper.js
+++ src/js/ui/NewCharting/KDEWrapper.js
@@ -4,11 +4,26 @@ import Slider from '@material-ui/core/Slider';
 import Input from '@material-ui/core/Input';
 import Checkbox from '@material-ui/core/Checkbox';
 
+const MIN_BANDWIDTH = 0.1
+const MAX_BANDWIDTH = 10
+const BANDWIDTH_STEP = 0.1
+
 export default function KDEWrapper(props) {
     let [enabled, setEnabled] = useState(false)
     let [bandwidth, setBandwidth] = useState(0.1);
 
     const handleChange = setter => (event, newValue) => setter(newValue);
+    const handleEvent = setter => event => {
+        let value = event.target.value;
+        setter(value === '' ? '' : Number(value));
+    };
+    const clampValue = (setter, value, min, max) => () => {
+        if (value < min) {
+            setter(min);
+        } else if (value > max) {
+            setter(max);
+        }
+    }
 
     return (
         <div>
@@ -23,21 +38,22 @@ export default function KDEWrapper(props) {
                     <Slider 
                         value={bandwidth}
                         onChange={handleChange(setBandwidth)}
-                        step={0.1}
-                        min={0.1}
-                        max={10}
+                        step={BANDWIDTH_STEP}
+                        min={MIN_BANDWIDTH}
+                        max={MAX_BANDWIDTH}
                         aria-labelledby="input-slider"
                     />
                 </Grid>
                 <Grid item xs>
                     <Input 
                         value={bandwidth}
-                        onChange={handleChange(setBandwidth)}
+                        onChange={handleEvent(setBandwidth)}
+                        onBlur={clampValue(setBandwidth, bandwidth, MIN_BANDWIDTH, MAX_BANDWIDTH)}
                         inputProps={{
                             type: 'number',
-                            step: 0.1,
-                            min: 0.1,
-                            max: 10,
+                            step: BANDWIDTH_STEP,
+                            min: MIN_BANDWIDTH,
+                            max: MAX_BANDWIDTH,
                             'aria-labelledby': "input-slider"
                         }}
                     />
