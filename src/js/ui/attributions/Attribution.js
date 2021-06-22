13ce26ec5cc517e2aa72e3ad398e973c2578ca72
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-22 13:56:00 -0600

    added neon

--- src/js/ui/attributions/Attribution.js
+++ src/js/ui/attributions/Attribution.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Button, Card, CardActionArea, CardActions, CardContent, CardMedia } from "@material-ui/core";
+import { Typography, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton } from "@material-ui/core";
+import LinkIcon from '@material-ui/icons/Link';
 
 const useStyles = makeStyles({
     root: {
@@ -16,30 +17,16 @@ export default function Attribution() {
 
     return <div>
         <Card className={classes.root}>
-            <CardActionArea>
-                {/* <CardMedia
-                    className={classes.media}
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
-                /> */}
+            <CardActionArea onClick={() => window.open(`https://www.neonscience.org/`, "_blank")}>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
+                        Neon
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
+                        The National Ecological Observatory Network is a program sponsored by the National Science Foundation and operated under cooperative agreement by Battelle. This material is based in part upon work supported by the National Science Foundation through the NEON Program.
                     </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Share
-                </Button>
-                <Button size="small" color="primary">
-                    Learn More
-                </Button>
-            </CardActions>
         </Card>
     </div>
 }
