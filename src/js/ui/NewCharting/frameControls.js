153ef0420baca52f3505935e666b2a394715deff
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2021-06-25 15:18:23 -0700

    moved all dropdowns over

--- src/js/ui/NewCharting/frameControls.js
+++ src/js/ui/NewCharting/frameControls.js
@@ -3,9 +3,17 @@ import ConstraintDropDown from "./constraintDropDown"
 
 export default function frameControls(props) {
 
+    let extraDropDown = ""
+
+    if(props.numDropDowns == 2){
+        extraDropDown = <ConstraintDropDown options={props.options} setConstraint={props.setConstraint2}></ConstraintDropDown>
+    }
 
     return(
-        <ConstraintDropDown options={props.options} setConstraint={props.setConstraint}></ConstraintDropDown>
+        <div>
+            <ConstraintDropDown options={props.options} setConstraint={props.setConstraint}></ConstraintDropDown>
+            {extraDropDown}
+        </div>
     )
 
 }
--- src/js/ui/NewCharting/makeFrame.js
+++ src/js/ui/NewCharting/makeFrame.js
@@ -40,17 +40,16 @@ export default function Frame(props) {
         case "line":
             frame = <LineGraph size={props.size} data={props.data} selected={constraint}></LineGraph>; break;
         case "piegraph":
-            frame = <div><ConstraintDropDown options={selectedConstraints} setConstraint={setConstraint}></ConstraintDropDown>
+            frame = <div><FrameControls options={selectedConstraints} setConstraint={setConstraint} numDropDowns={1}></FrameControls>
                 <PieGraph size={props.size} data={props.data} selected={constraint}></PieGraph></div>; break;
         case "scatterplot":
             frame =
                 <div>
-                    <ConstraintDropDown options={selectedConstraints} setConstraint={setConstraint}></ConstraintDropDown>
-                    <ConstraintDropDown options={selectedConstraints} setConstraint={setConstraint2}></ConstraintDropDown>
+                    <FrameControls options={selectedConstraints} setConstraint={setConstraint} setConstraint2={setConstraint2} numDropDowns={2}></FrameControls>
                     <ScatterPlot size={props.size} data={props.data} selected={[constraint, constraint2]}></ScatterPlot>
                 </div>; break;
         case "boxplot":
-            frame = <div><ConstraintDropDown options={selectedConstraints} setConstraint={setConstraint}></ConstraintDropDown>
+            frame = <div><FrameControls options={selectedConstraints} setConstraint={setConstraint} numDropDowns={1}></FrameControls>
                 <BoxPlot size={props.size} data={props.data} selected={constraint}></BoxPlot></div>; break;
         case "radarchart":
             frame = <div>
