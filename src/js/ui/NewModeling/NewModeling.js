24392add7ac033bf22e8f2367f66328cbd5990f3
Matt Young <honeybadgerofdoom@gmail.com>
2021-06-29 15:28:07 -0600

    Memo for the Modeling tab as well

--- src/js/ui/NewModeling/NewModeling.js
+++ src/js/ui/NewModeling/NewModeling.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {componentIsRendering} from "../TabSystem"
 import Typography from "@material-ui/core/Typography";
 
-export default function NewModeling() {
+export default React.memo(function NewModeling() {
     if(componentIsRendering) console.log("|NewModeling|");
     return (
         <Typography style={{textAlign: 'center', fontSize: '18'}}>Coming Soon</Typography>
     )
-}
+})
