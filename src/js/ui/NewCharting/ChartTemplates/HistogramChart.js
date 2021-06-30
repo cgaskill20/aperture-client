228a0aa8dd0acc0cc69266843e0a1a91694e2f79
psmith36 <psmith36@rams.colostate.edu>
2021-06-30 15:53:17 -0600

    package chagnes

--- src/js/ui/NewCharting/ChartTemplates/HistogramChart.js
+++ src/js/ui/NewCharting/ChartTemplates/HistogramChart.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import * as d3 from '../../../third-party/d3.min.js';
 import KernelDensityEstimator from '../../../library/charting/kernelDensityEstimator';
+import d3tip from 'd3-tip';
+
+// A tooltip-compatible object that does nothing.
+// Used as the initial state of the tooltip, so that React does not crash.
+const dummyTip = {
+    element: {
+        show: () => {},
+        hide: () => {}
+    }
+}
 
 export default function HistogramGraph(props) {
 
     let svgRef = React.createRef();
     let [margin, setMargin] = useState({ top: 0, right: 10, bottom: 250, left: 20 });
     let [kde, setKde] = useState(new KernelDensityEstimator());
+    let [tip, setTip] = useState(dummyTip)
 
     let setup = () => {
         let svg = d3.select(svgRef.current);
@@ -15,6 +26,23 @@ export default function HistogramGraph(props) {
         svg.append("g").attr("id", "xAxis");
         svg.append("g").attr("id", "yAxis");
         svg.append("path").attr("id", "kdecurve");
+
+        // Create a tip attached to our svg element
+        let svgTip = d3tip(svg);
+
+        // Give a class for CSS styling
+        svgTip.attr('class', 'chart-tooltip');
+        
+        svgTip.html(d => d);
+
+        // We cannot simply set the tip state to `svgTip', since `svgTip'
+        // happens to be a function, and if a function is passed into any state setter 
+        // then React will just call it with no agruments, ruining everything.
+        // To solve this, I wrap `svgTip' in an object.
+        setTip({ element: svgTip });
+
+        // This puts the tooltip into the dom
+        svg.call(svgTip);
     }
 
     let prepareData = data => {
@@ -58,11 +86,15 @@ export default function HistogramGraph(props) {
             .selectAll("rect")
                 .attr("fill", d => "steelblue")
             .data(bins)
+                .on('mouseover', tip.element.show)
+                .on('mouseout', tip.element.hide)
             .join("rect")
                 .attr("x", d => x(d.x0) + 1)
                 .attr("width", d => Math.max(0, x(d.x1) - x(d.x0) - 1))
                 .attr("y", d => y(d.length))
-                .attr("height", d => y(0) - y(d.length));
+                .attr("height", d => y(0) - y(d.length))
+                // Need to call .on('mouseover') and on('mouseout') on each
+                // d3 element you want to show the tooltip!
 
 
         if (props.kdeEnabled) {
@@ -83,7 +115,9 @@ export default function HistogramGraph(props) {
             svg.select("path#kdecurve")
                 .attr("display", "none");
         }
+
     }
+
 
     useEffect(setup, []);
     useEffect(rerender.bind(this, props.size.width, props.size.height));
