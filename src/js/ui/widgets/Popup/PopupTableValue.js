86581333b38e2cc41abbcad0d718ba59b4baf23c
Matt Young <honeybadgerofdoom@gmail.com>
2021-08-12 10:32:29 -0600

    will be yeeted

--- src/js/ui/widgets/Popup/PopupTableValue.js
+++ src/js/ui/widgets/Popup/PopupTableValue.js
@@ -57,11 +57,10 @@ You may add Your own copyright statement to Your modifications and may provide a
 END OF TERMS AND CONDITIONS
 */
 
-import React, { useState } from "react";
+import React from "react";
 import { valueToDisplay } from "./PopupUtils";
-import { Table, TableContainer, TableHead, TableCell, TableRow, TableBody, Paper, makeStyles, Tooltip } from "@material-ui/core";
+import {makeStyles, Tooltip } from "@material-ui/core";
 import PaletteIcon from '@material-ui/icons/Palette';
-import useHover from "../../hooks/useHover";
 
 const useStyles = makeStyles({
     root: {
@@ -70,7 +69,6 @@ const useStyles = makeStyles({
 });
 
 export default function PopupTableValue({ obj, keyValue, value, isHovered, entryProperties }) {
-    //console.log({ obj, keyValue })
     const classes = useStyles()
 
     const colorIcon = () => {
@@ -88,4 +86,5 @@ export default function PopupTableValue({ obj, keyValue, value, isHovered, entry
         {valueToDisplay(obj, keyValue, value)}
         {colorIcon()}
     </div>
+
 }
