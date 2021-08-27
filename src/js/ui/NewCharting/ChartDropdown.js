6ecd8f4677a0661a8fba246af5e569fa5e94ce0b
Matt Young <honeybadgerofdoom@gmail.com>
2021-08-27 13:02:37 -0600

    Dropdown boxes consistent styling

--- src/js/ui/NewCharting/ChartDropdown.js
+++ src/js/ui/NewCharting/ChartDropdown.js
@@ -64,11 +64,10 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-
 const useStyles = makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1),
-        minWidth: 150,
+        minWidth: 250,
         float: "left",
     },
     selectEmpty: {
@@ -89,14 +88,14 @@ export default function ChartDropdown(props) {
        age = ""
     }
 
-
     return (
         <div>
-            <FormControl className={classes.formControl}>
+            <FormControl variant="outlined" className={classes.formControl}>
                 <InputLabel id="demo-simple-select-label">Select Constraint</InputLabel>
                 <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
+                    label="Select Constraint"
                     value={age}
                     onChange={handleChange}
                 >
