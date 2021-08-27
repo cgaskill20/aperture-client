1951575bcb1ac94b7afec650d622addb4872c85d
Matt Young <honeybadgerofdoom@gmail.com>
2021-08-27 16:05:35 -0600

    axes names change for scatterplots between tract and county

--- src/js/ui/NewCharting/FrameControls.js
+++ src/js/ui/NewCharting/FrameControls.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ToggleSwitch from './ToggleSwitch';
@@ -14,34 +14,37 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FrameControls(props) {
     const classes = useStyles();
+    const dropdownNameOptions = ["Constraint", "X-Axis County", "X-Axis Tract", "Y-Axis County", "Y-Axis Tract"];
     const [tractOrCounty, setTractOrCounty] = useState(true);
+    const [dropdownName1, setDropdownName1] = useState(dropdownNameOptions[1]);
+    const [dropdownName2, setDropdownName2] = useState(dropdownNameOptions[3]);
     let menuOptions = props.options;
+
+    function handleToggle() {
+        setTractOrCounty(!tractOrCounty);
+        if(tractOrCounty) {
+            setDropdownName1(dropdownNameOptions[2]);
+            setDropdownName2(dropdownNameOptions[4]);
+        }
+        else {
+            setDropdownName1(dropdownNameOptions[1]);
+            setDropdownName2(dropdownNameOptions[3]);
+        }
+    }
 
     function renderToggle() {
         if(props.selector){
-            if(tractOrCounty){
-                menuOptions = props.options[0]
-            }
-            else {
-                menuOptions = props.options[1]
-            }
-            return <Grid item><ToggleSwitch tractOrCounty={tractOrCounty} setTractOrCounty={() => setTractOrCounty(!tractOrCounty)}></ToggleSwitch></Grid>
+            menuOptions = tractOrCounty ? props.options[0] : props.options[1];
+            return <Grid item><ToggleSwitch tractOrCounty={tractOrCounty} setTractOrCounty={() => handleToggle()}></ToggleSwitch></Grid>
         }
     }
 
-    function renderDropdown(first, axis) {
+    function renderDropdown(first) {
         const setConstraint = first ? props.setConstraint : props.setConstraint2;
-        const dropdownName = getDropdownName(axis);
+        const name = first ? dropdownName1 : dropdownName2;
         if(props.numDropDowns > 0 && (first || props.numDropDowns === 2)) {
-            return <Grid item><ChartDropdown dropdownName={dropdownName} options={menuOptions} setConstraint={setConstraint}></ChartDropdown></Grid>
+            return <Grid item><ChartDropdown dropdownName={name} options={menuOptions} setConstraint={setConstraint}></ChartDropdown></Grid>
         }
-    }
-
-    function getDropdownName(axis) {
-        if(props.type === "scatterplot") {
-            return props.tractvCounty ? axis + " Tract" : axis + " County";
-        }
-        return "Constraint"
     }
 
     function renderCloseButton() {
@@ -61,8 +64,8 @@ export default function FrameControls(props) {
                 alignItems="center"
             >
                 {renderToggle()}
-                {renderDropdown(false, "Y-Axis")}
-                {renderDropdown(true, "X-Axis")}
+                {renderDropdown(true)}
+                {renderDropdown(false)}
                 {renderCloseButton()}
             </Grid>
         </div>
--- src/js/ui/NewCharting/ToggleSwitch.js
+++ src/js/ui/NewCharting/ToggleSwitch.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Switch from "@material-ui/core/Switch";
 import Grid from "@material-ui/core/Grid";
 import PeopleIcon from '@material-ui/icons/People';
-import PersonIcon from '@material-ui/icons/Person';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import {Tooltip} from "@material-ui/core";
 
 export default function ToggleSwitch(props){
     return (
         <div><Grid component="label" container alignItems="center">
-            <Grid item><PersonIcon/></Grid>
+            <Grid item><Tooltip title="Chart Tract Datasets"><PeopleIcon/></Tooltip></Grid>
             <Grid item>
                 <Switch
+                    color="primary"
                     checked={props.tractOrCounty}
                     onChange={props.setTractOrCounty}
                     value="Tract or County"
                 />
             </Grid>
-            <Grid item><PeopleIcon/></Grid>
+            <Grid item><Tooltip title="Chart County Datasets"><AccountBalanceIcon/></Tooltip></Grid>
         </Grid></div>
     )
 }
