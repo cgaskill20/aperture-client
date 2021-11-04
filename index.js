2af5d189ac59618eda1ea33d639338ffa6a36fc5
Daniel Reynolds <danielreynolds101@gmail.com>
2021-11-04 14:08:43 -0600

    clear old stuff, add query

--- index.js
+++ index.js
@@ -56,153 +56,165 @@ You may add Your own copyright statement to Your modifications and may provide a
 
 END OF TERMS AND CONDITIONS
 */
-import "./src/css/variables.css";
-import "./src/css/darkModeSwitch.css";
-import "./src/css/imageStyles.css";
-import "./src/css/dataExploration.css";
-import "./src/css/queries.css";
-import "./src/css/modeling.css";
-import "./src/css/charts.css";
-import "./src/css/main.css";
-import "./src/css/leafletOverrides.css";
-import "./src/css/bootstrapOverrides.css";
-import "./src/css/third-party/nouislider.min.css";
-import "./src/css/third-party/bootstrap4-toggle.min.css";
-import "./src/css/third-party/bootstrap.min.css";
-import "./src/css/third-party/MarkerCluster.css";
-import "./src/css/third-party/MarkerCluster.Default.css";
-
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+console.log("e")
+import { sustain_querier } from "./src/js/grpc/GRPC_Querier/grpc_querier.js";
+const querier = sustain_querier()
+const stream = querier.echoQuery("state_geo", "[]");
+stream.on('data', (res) => {
+    const data = JSON.parse(res.getData());
+    console.log({data})
+});
+stream.on('end', () => {
+    console.log("done")
 });
 
-import ChartSystem from "./src/js/library/charting/chartSystem"
-import ModelMenu from "./src/js/ui/modeling/components/ModelMenu";
-import AutoQuery from "./src/js/library/autoQuery.js";
-import RenderInfrastructure from "./src/js/library/renderInfrastructure.js";
-import AutoMenu from "./src/js/library/autoMenu.js";
-// import DefensiveOptimization from "./src/js/ui/DefensiveOptimization";
-import Root from "./src/js/ui/Root";
-import getOverwrite from "./src/js/static/menuOverwrite";
+// import "./src/css/variables.css";
+// import "./src/css/darkModeSwitch.css";
+// import "./src/css/imageStyles.css";
+// import "./src/css/dataExploration.css";
+// import "./src/css/queries.css";
+// import "./src/css/modeling.css";
+// import "./src/css/charts.css";
+// import "./src/css/main.css";
+// import "./src/css/leafletOverrides.css";
+// import "./src/css/bootstrapOverrides.css";
+// import "./src/css/third-party/nouislider.min.css";
+// import "./src/css/third-party/bootstrap4-toggle.min.css";
+// import "./src/css/third-party/bootstrap.min.css";
+// import "./src/css/third-party/MarkerCluster.css";
+// import "./src/css/third-party/MarkerCluster.Default.css";
 
-//idek what to do with theses files, things break if I try to make them import specific things
-import "./src/js/third-party/leaflet.markercluster.js";
-require("./src/js/third-party/popper.min.js");
-require("./src/js/third-party/bootstrap4-toggle.min.js");
-require("bootstrap");
-require("./src/js/library/smartQuerier.js");
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 
-export let finalData;
-const PAGE_URL = window.location.href;
-const DEV = PAGE_URL.includes("localhost") || PAGE_URL.includes("127.0.0.1");
-if (DEV && !localStorage.getItem("noDev")) {
-    console.log("Aperture client set to DEV mode.")
-}
-
-//--------------
-const standardTiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    maxZoom: 18
-});
-
-const topoTiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    subdomains: 'abcd',
-    minZoom: 0,
-    maxZoom: 18,
-    ext: 'png'
-});
-
-const satelliteTiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    maxZoom: 18
-});
-
-const baseMaps = {
-    "Default": standardTiles,
-    "Topography": topoTiles,
-    "Satellite": satelliteTiles
-};
-
-const map = L.map('map2', {
-    renderer: L.canvas(),
-    minZoom: 3,
-    fullscreenControl: true,
-    inertia: false,
-    timeDimension: false,
-    zoomControl: false
-});
-window.map = map;
-map.setView([40.573733, -105.086559], 11);
-
-L.control.layers(baseMaps).addTo(map);
-standardTiles.addTo(map);
-
-const zoomControl = L.control.zoom({ position: "topright" }).addTo(map);
-
-var markers = L.markerClusterGroup({
-    showCoverageOnHover: false,
-    spiderfyOnMaxZoom: true,
-    maxClusterRadius: 55,
-    animate: false
-});
-map.addLayer(markers);
-
-const dataExplorationGroup = L.featureGroup().addTo(map);
-const dataModelingGroup = L.featureGroup();
-window.dataExplorationGroup = dataExplorationGroup;
-window.dataModelingGroup = dataModelingGroup;
-
-import Worker from "./src/js/library/geometryLoaderWorker.js"
-const preloadStatusContainer = document.getElementById("preloadStatus");
-let bgTractId = "bgTract";
-let bgCountyId = "bgCounty";
-const backgroundTract = new Worker();
-const backgroundCounty = new Worker();
-globalThis.loaders = [
-    {
-        id: bgTractId,
-        loader: backgroundTract,
-        collection: "tract_geo_140mb_no_2d_index"
-    },
-    {
-        id: bgCountyId,
-        loader: backgroundCounty,
-        collection: "county_geo_30mb_no_2d_index"
-    }
-];
-window.backgroundTract = backgroundTract;
-window.backgroundCounty = backgroundCounty;
-
-const overwrite = getOverwrite();
-
-window.renderInfrastructure = new RenderInfrastructure(map, markers, dataExplorationGroup, overwrite, {
-    queryAlertText: document.getElementById('queryInfoText'),
-    maxElements: 10000,
-    maxLayers: 20,
-    simplifyThreshold: 0.0001
-});
-window.chartSystem = new ChartSystem(map, "src/json/graphPriority.json", window.renderInfrastructure);
-
-const uiRoot = document.getElementById("ui-root");
-ReactDOM.render((<Root map={map} overwrite={overwrite}/>), uiRoot);
-
-// const modelContainer = document.getElementById("model-container");
-// ReactDOM.render((<ModelMenu/>), modelContainer);
-
-
-// import {closeNav} from "./src/js/static/navButtons";
-import "./src/js/static/darkMode.js";
-
-// map.on('click', function () {
-//     closeNav();
+// import L from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+// delete L.Icon.Default.prototype._getIconUrl;
+// L.Icon.Default.mergeOptions({
+//     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//     iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//     shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 // });
+
+// import ChartSystem from "./src/js/library/charting/chartSystem"
+// import ModelMenu from "./src/js/ui/modeling/components/ModelMenu";
+// import AutoQuery from "./src/js/library/autoQuery.js";
+// import RenderInfrastructure from "./src/js/library/renderInfrastructure.js";
+// import AutoMenu from "./src/js/library/autoMenu.js";
+// // import DefensiveOptimization from "./src/js/ui/DefensiveOptimization";
+// import Root from "./src/js/ui/Root";
+// import getOverwrite from "./src/js/static/menuOverwrite";
+
+// //idek what to do with theses files, things break if I try to make them import specific things
+// import "./src/js/third-party/leaflet.markercluster.js";
+// require("./src/js/third-party/popper.min.js");
+// require("./src/js/third-party/bootstrap4-toggle.min.js");
+// require("bootstrap");
+// require("./src/js/library/smartQuerier.js");
+
+// export let finalData;
+// const PAGE_URL = window.location.href;
+// const DEV = PAGE_URL.includes("localhost") || PAGE_URL.includes("127.0.0.1");
+// if (DEV && !localStorage.getItem("noDev")) {
+//     console.log("Aperture client set to DEV mode.")
+// }
+
+// //--------------
+// const standardTiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+//     maxZoom: 18
+// });
+
+// const topoTiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+//     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     subdomains: 'abcd',
+//     minZoom: 0,
+//     maxZoom: 18,
+//     ext: 'png'
+// });
+
+// const satelliteTiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+//     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+//     maxZoom: 18
+// });
+
+// const baseMaps = {
+//     "Default": standardTiles,
+//     "Topography": topoTiles,
+//     "Satellite": satelliteTiles
+// };
+
+// const map = L.map('map2', {
+//     renderer: L.canvas(),
+//     minZoom: 3,
+//     fullscreenControl: true,
+//     inertia: false,
+//     timeDimension: false,
+//     zoomControl: false
+// });
+// window.map = map;
+// map.setView([40.573733, -105.086559], 11);
+
+// L.control.layers(baseMaps).addTo(map);
+// standardTiles.addTo(map);
+
+// const zoomControl = L.control.zoom({ position: "topright" }).addTo(map);
+
+// var markers = L.markerClusterGroup({
+//     showCoverageOnHover: false,
+//     spiderfyOnMaxZoom: true,
+//     maxClusterRadius: 55,
+//     animate: false
+// });
+// map.addLayer(markers);
+
+// const dataExplorationGroup = L.featureGroup().addTo(map);
+// const dataModelingGroup = L.featureGroup();
+// window.dataExplorationGroup = dataExplorationGroup;
+// window.dataModelingGroup = dataModelingGroup;
+
+// import Worker from "./src/js/library/geometryLoaderWorker.js"
+// const preloadStatusContainer = document.getElementById("preloadStatus");
+// let bgTractId = "bgTract";
+// let bgCountyId = "bgCounty";
+// const backgroundTract = new Worker();
+// const backgroundCounty = new Worker();
+// globalThis.loaders = [
+//     {
+//         id: bgTractId,
+//         loader: backgroundTract,
+//         collection: "tract_geo_140mb_no_2d_index"
+//     },
+//     {
+//         id: bgCountyId,
+//         loader: backgroundCounty,
+//         collection: "county_geo_30mb_no_2d_index"
+//     }
+// ];
+// window.backgroundTract = backgroundTract;
+// window.backgroundCounty = backgroundCounty;
+
+// const overwrite = getOverwrite();
+
+// window.renderInfrastructure = new RenderInfrastructure(map, markers, dataExplorationGroup, overwrite, {
+//     queryAlertText: document.getElementById('queryInfoText'),
+//     maxElements: 10000,
+//     maxLayers: 20,
+//     simplifyThreshold: 0.0001
+// });
+// window.chartSystem = new ChartSystem(map, "src/json/graphPriority.json", window.renderInfrastructure);
+
+// const uiRoot = document.getElementById("ui-root");
+// ReactDOM.render((<Root map={map} overwrite={overwrite}/>), uiRoot);
+
+// // const modelContainer = document.getElementById("model-container");
+// // ReactDOM.render((<ModelMenu/>), modelContainer);
+
+
+// // import {closeNav} from "./src/js/static/navButtons";
+// import "./src/js/static/darkMode.js";
+
+// // map.on('click', function () {
+// //     closeNav();
+// // });
 
